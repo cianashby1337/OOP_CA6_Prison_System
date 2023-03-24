d4336bd5c8e99f0de765550680087429b77a2c16
Cian <cianashby1337@gmail.com>
2023-03-24 20:25:56 +0000

    Feature 1 Complete, starter Menu System added

--- src/BusinessObjects/Main.java
+++ src/BusinessObjects/Main.java
@@ -1,7 +1,49 @@
 package BusinessObjects;
 
+import DAOs.MySqlPrisonerDao;
+import DAOs.PrisonerDaoInterface;
+import DTOs.Prisoner;
+import Exceptions.DaoException;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PrisonerDaoInterface IUserDao = new MySqlPrisonerDao();
+        Scanner userInput = new Scanner(System.in);
+        int choice;
+        while (true) {
+            System.out.println("""
+                    Choose an option from below:
+                    0 - Exit program
+                    1 - Display all prisoners""");
+            try {
+                choice = userInput.nextInt();
+                switch (choice) {
+                    case 0:
+                        System.out.println("Exiting program...");
+                        return;
+                    case 1:
+                        try {
+                            List<Prisoner> prisoners = IUserDao.findAllPrisoners();
+                            System.out.println(prisoners);
+                        }
+                        catch (DaoException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid number, please try again");
+                        break;
+                }
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Incorrect entry, please try again");
+                userInput.next();
+            }
+        }
+
     }
 }
