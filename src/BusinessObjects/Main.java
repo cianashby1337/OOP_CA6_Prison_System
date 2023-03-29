5684a7eae9a9ed27f34981e6bf188d5b9c48a64b
Cian <cianashby1337@gmail.com>
2023-03-29 01:12:27 +0100

    Feature 6: cachedId hash set created

--- src/BusinessObjects/Main.java
+++ src/BusinessObjects/Main.java
@@ -15,6 +15,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         PrisonerDaoInterface IPrisonerDao = new MySqlPrisonerDao();
+        HashSet<Integer> cachedIds = new HashSet<>();
 
         Scanner userInput = new Scanner(System.in);
         int choice;
