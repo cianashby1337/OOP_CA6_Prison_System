4b6edb892fa867c98fe74b49e98b0ed422d0a5dd
Cian <cianashby1337@gmail.com>
2023-05-01 01:05:53 +0100

    Unit test for deletePrisonerById() added

--- src/DAOs/MySqlPrisonerDaoTest.java
+++ src/DAOs/MySqlPrisonerDaoTest.java
@@ -18,11 +18,17 @@ class MySqlPrisonerDaoTest {
     }
 
     @org.junit.jupiter.api.Test
-    void findPrisonerById() {
+    void findPrisonerById() throws DaoException {
+        System.out.println("Test 2 - Assert that the found prisoner's ID no. matches the searched number (1)");
+        Prisoner testPrisoner = IPrisonerDAO.findPrisonerById(1);
+        if (testPrisoner == null) fail("Returned value was null. Ensure the database contains a prisoner with an id of 1 before assuming failure");
+        assertTrue(testPrisoner.getPrisoner_id() == 1,"A prisoner was returned, but an ID mismatch was detected");
     }
 
     @org.junit.jupiter.api.Test
-    void deletePrisonerById() {
+    void deletePrisonerById() throws DaoException {
+        System.out.println("Test 3 - Assert a removal has taken place within a list (seeks prisoner with id no. 1)");
+        assertEquals(1,IPrisonerDAO.deletePrisonerById(1),"Ensure there was a prisoner with an id of 1 to be removed, before assuming failure");
     }
 
     @org.junit.jupiter.api.Test
